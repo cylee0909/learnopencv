fb9a9426e87f1dd515eb585a06ad17b8ec7ce987
cylee <cylee0909@qq.com>
2017-08-28 22:13:22 +0800

    init

---
+++ src/ShowImg.cpp
@@ -0,0 +1,30 @@
+//============================================================================
+// Name        : TestCpp.cpp
+// Author      : cylee
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <opencv2/opencv.hpp>  //头文件
+using namespace cv;
+//包含cv命名空间
+using namespace std;
+
+int main() {
+	// 【1】读入一张图片，载入图像
+	Mat srcImage = imread("img1.jpg");
+	// 【2】显示载入的图片
+	imshow("【原始图】", srcImage);
+
+	//进行腐蚀操作
+	Mat element = getStructuringElement(MORPH_RECT, Size(15, 15));
+	Mat dstImage;
+	erode(srcImage, dstImage, element);
+	//显示效果图
+	imshow("【效果图】腐蚀操作", dstImage);
+	waitKey(0);
+	// 【3】等待任意按键按下
+	return 0;
+}
